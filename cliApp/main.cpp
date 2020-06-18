609696db7fe4cf442d6c0e46c89a879ccb97d965
joschua <joschua.fink@gmx.de>
2020-06-18 18:01:45 +0200

    use vector to store points

--- cliApp/main.cpp
+++ cliApp/main.cpp
@@ -1,7 +1,9 @@
 #include <cmath>
 #include <iostream>
+// use math defines for getting pi as M_PI
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <vector> 
 #include "points.h"
 
 using namespace std;
@@ -15,6 +17,7 @@ int main ( int argc, char *argv[]) {
 	unsigned int numPoints = numCitys * numPointFactor;
 	double deltaAngle = 2*PI / numPoints;
 
+    // output initial values
 	cout << "Number of Citys: " << numCitys << endl;
 	cout << "Radius: " << radius << endl;
 	cout << "numPointFactor: " << numPointFactor << endl;
@@ -22,10 +25,19 @@ int main ( int argc, char *argv[]) {
 	cout << "Delta angle: " << deltaAngle << endl;
 	cout << "\nInitial coordinates of points: " << endl;
 
-	unsigned int count = 0;
+    // dynamically allocate vector
+    vector<Point> *points = new vector<Point>;
+
+    // put all points into the points vector in form of a circle
 	for (double angle = 0; angle <= 2*PI; angle+=deltaAngle) {
-		Point tmppoint( radius * cos( angle ),  radius * sin( angle ) );
-		cout << count++ << ": x = "<< tmppoint.x << ", \ty = " << tmppoint.y << endl;
+		points->push_back( Point( radius * cos( angle ),  radius * sin( angle ) ) );
 	}
 
+	unsigned int pointCount = 0;
+    // traverse points
+    for(std::vector<Point>::iterator it = points->begin(); it != points->end(); ++it) {
+            cout << "Point " << pointCount++ << ":\tx:" << it->x << ",\ty:" << it->y << endl;
+    }
+
+    delete points; // delete points vector
 }
