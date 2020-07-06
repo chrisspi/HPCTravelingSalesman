f3493d223201f515c1d6f440b601f6b921e2ae02
Stefan Chalupka <50046445+chalupka95@users.noreply.github.com>
2020-07-06 21:33:34 +0200

    Update main.cpp

--- HPC_TSP_GUI/main.cpp
+++ HPC_TSP_GUI/main.cpp
@@ -7,24 +7,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-/*
-    TSPVector vec(0,4,0);
-    TSPVector vec2(3,0,0);
-    std::cout << "X: " << vec.x << " Y: " << vec.y << std::endl;
-    std::cout << vec.magnitude() << std::endl;
-    std::cout << "X: " << vec2.x << " Y: " << vec2.y << std::endl;
-    std::cout << vec2.magnitude() << std::endl;
-    vec += vec2;
-    std::cout << "X: " << vec.x << " Y: " << vec.y << std::endl;
-    std::cout << vec.magnitude() << std::endl;
-
-    TSP tsp("/home/stefan/Dokumente/HPCTravelingSalesman/cliApp/berlin52.tsp");
-    std::vector<City>* cities = tsp.getCities();
-    for( int i = 0; i < cities->size(); i++ ) {
-        std::cout << "X: " << cities->at(i).x << " Y: " << cities->at(i).y << std::endl;
-        std::cout << cities->at(i).magnitude() << std::endl;
-        }
-*/
-    
     return a.exec();
 }
